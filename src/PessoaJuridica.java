71e4a309732c246838523f9626d034e0fcaeef3e
Vinicius Gonçalves Aragão Costa <131018908+ViniciusGACosta@users.noreply.github.com>
2023-05-02 10:40:21 -0300

    Update PessoaJuridica.java

--- src/PessoaJuridica.java
+++ src/PessoaJuridica.java
@@ -1,6 +1,13 @@
 
 public class PessoaJuridica extends Pessoa implements Autenticacao {
+	private String cnpj;
+	public String getCnpj() {
+    		return cnpj;
+ 	 }
 
+ 	 public void setCnpj(String cnpj) {
+    		this.cnpj = cnpj;
+  	}
 	@Override
 	public boolean autentica() {
 		// TODO Auto-generated method stub

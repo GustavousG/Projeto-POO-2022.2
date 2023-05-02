e5a3aeb1700b32e5cfe5190437804b55191a88b3
Vinicius Gonçalves Aragão Costa <131018908+ViniciusGACosta@users.noreply.github.com>
2023-05-02 10:37:51 -0300

    Update PessoaFisica.java

--- src/PessoaFisica.java
+++ src/PessoaFisica.java
@@ -1,6 +1,13 @@
 
 public class PessoaFisica extends Pessoa implements Autenticacao{
+	private String cpf;
+	public String getCpf() {
+    		return cpf;
+ 	 }
 
+ 	 public void setCpf(String cpf) {
+    		this.cpf = cpf;
+  	}
 	@Override
 	public boolean autentica() {
 		// TODO Auto-generated method stub

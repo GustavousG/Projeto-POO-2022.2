668623ca570eba019d4759420091702a1a210046
Vinicius Gonçalves Aragão Costa <131018908+ViniciusGACosta@users.noreply.github.com>
2023-05-07 16:07:08 -0300

    Add files via upload

---
+++ Urna/src/main/java/grupo/urna/CarregarPessoas.java
@@ -0,0 +1,34 @@
+package grupo.urna;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarregarPessoas {
+    private List<Pessoa> pessoas = new ArrayList<>();
+    
+    public CarregarPessoas(String caminhoArquivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha = br.readLine();
+            while (linha != null) {
+                String[] campos = linha.split(";");
+                if (campos[0].equals("0")) {
+                    PessoaFisica pessoa = new PessoaFisica(Integer.parseInt(campos[1]), campos[2], campos[3], campos[4]);
+                    pessoas.add(pessoa);
+                } else if (campos[0].equals("1")) {
+                    PessoaJuridica pessoa = new PessoaJuridica(Integer.parseInt(campos[1]), campos[2], campos[3], campos[4]);
+                    pessoas.add(pessoa);
+                }
+                linha = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo: " + e.getMessage());
+        }
+    }
+    
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+}
